5305101717c429f3a96dc79ce7aeb0c96a101154
Starry <140034460+meti-liu@users.noreply.github.com>
2023-12-27 16:30:52 +0800

    1

--- Dino/Dino.cpp
+++ Dino/Dino.cpp
@@ -3,6 +3,8 @@
 #include<conio.h>
 #include<vector>//采用动态数组来管理持续增加的cactus，cloud或bird
 #include<ctime>//用于随机数生成
+#include<map>//用于碰撞检测中的批量初始化
+#include<cmath>
 
 #pragma comment(lib,"winmm.lib")
 
@@ -13,8 +15,8 @@ int speed_increase = 4;
 int lastSpeedIncreaseScore = 0; // 新增一个变量，记录上次增加速度时的分数
 const int width = 896;//地图长宽
 const int height = 512;
-const int SlowStep =4;
-const int FastStep =2;
+const int SlowStep = 4;
+const int FastStep = 2;
 const int SlowWing = 5;//我们把一些参数都设置为全局变量，这样后续微调的时候
 //只需要在最前面改就够了
 const int FastWing = 3;
@@ -33,6 +35,7 @@ IMAGE img_background;
 IMAGE img_cac1, img_cac2, img_cac3, img_cac4, img_cac5, img_cac6;//六张不同的仙人掌图片
 IMAGE img_bird1, img_bird2;
 bool End = false;//撞到仙人掌或鸟，game over
+//int Life = 3;//撞到仙人掌或鸟，血量-1
 int dino_index;//有两张恐龙图片，循环播放，这里记录下标
 int bird_index;//实现鸟的翅膀扇动，循环播放
 //to be done
@@ -59,9 +62,25 @@ public:
 	float x, y;
 	float vx;//相对运动，障碍物向左，这里vx应该是负的
 	int Cactype;
-	Cactus() : x(0), y(0), Cactype(1) {}//先重载构造一个无参数的Cactus，否则vector的定义会报错
-	Cactus(float x,float y,int Cactype):x(x),y(y),Cactype(Cactype){}//构造函数+初始化仙人掌相关数据
-	//包括其xy坐标，移动速度vx，是哪种类型的仙人掌
+
+
+	//这是碰撞检测的参数，基本上是最难的部分
+	//我们注意到恐龙的跳跃或者说它与仙人掌碰撞的临界坐标近似可以模拟成一个抛物线
+	//我们用半圆的思路去处理碰撞，同时加上几个边界使其尽可能接近抛物线
+
+	int offset_x, offset_y;//半圆圆心相对于cactus坐标的偏移量，这是图片左右不对称导致的
+	int radius;//半圆半径
+	int leftBoundOffset;  // 左侧限制边界的偏移
+	int rightBoundOffset; // 右侧限制边界的偏移
+	int upperBoundOffset; // 上方限制边界的偏移
+	Cactus() : x(0), y(0), Cactype(1), offset_x(0), offset_y(0), radius(0), leftBoundOffset(0), rightBoundOffset(0), upperBoundOffset(0) {}
+	//先重载构造一个无参数的Cactus，否则vector的定义会报错
+	Cactus(float x, float y, int Cactype, int ox, int oy, int r, int lbo, int rbo, int ubo)
+		: x(x), y(y), Cactype(Cactype), offset_x(ox), offset_y(oy), radius(r), leftBoundOffset(lbo), rightBoundOffset(rbo), upperBoundOffset(ubo) {}
+	//构造函数+初始化仙人掌相关数据
+	//包括其xy坐标，是哪种类型的仙人掌
+	//每个类型仙人掌有不同的边界框（半圆，抛物线）
+
 	void draw()
 	{
 		switch (Cactype)
@@ -69,14 +88,38 @@ public:
 		case 1:putimage1(x, y, &img_cac1); break;
 		case 2:putimage1(x, y, &img_cac2); break;
 		case 3:putimage1(x, y, &img_cac3); break;
-		case 4:putimage1(x, y+20, &img_cac4); break;//因为加载图片是从x，y开始向右下角加载的
-		case 5:putimage1(x, y+20, &img_cac5); break;//所以+20是避免小仙人掌加载时悬在空中
-		case 6:putimage1(x, y+20, &img_cac6); break;
+		case 4:putimage1(x, y, &img_cac4); break;
+		case 5:putimage1(x, y, &img_cac5); break;
+		case 6:putimage1(x, y, &img_cac6); break;
 		}
 	}
 	void move()
 	{
-		x -= cac_speed;
+		x -= cac_speed;//相对运动
+	}
+
+	bool crack(int x_dino, int y_dino) const
+		//定义碰撞函数，判断恐龙坐标是否在碰撞框内
+	{
+		int ox = x + offset_x;//ox,oy是半圆圆心坐标
+		int oy = y + offset_y;//offset是偏移量，这是图片左右不对称导致的
+
+		int dx = abs(x_dino - ox);
+		int dy = abs(y_dino - oy);
+
+		int distanceSquared = dx * dx + dy * dy;
+
+		int leftBound = ox - leftBoundOffset;
+		int rightBound = ox + rightBoundOffset;
+		int upperBound = oy - upperBoundOffset;
+
+		return (distanceSquared <= radius * radius) &&
+			(x_dino > leftBound) &&
+			(x_dino < rightBound) &&
+			(y_dino > upperBound) && (y_dino < oy);
+		//需要同时满足2个条件才能被判定为在碰撞区域内
+		//1.在圆内  2.未超过上下左右边界
+		//这样的碰撞区域就可模拟抛物线
 	}
 };
 
@@ -86,8 +129,8 @@ public:
 	float x, y;
 	float vx;
 	int Birdtype;//高鸟和低鸟
-	Bird() :x(0),Birdtype(1){}
-	Bird(float x,int Birdtype) :x(x),Birdtype(Birdtype){}
+	Bird() :x(0), Birdtype(1) {}
+	Bird(float x, int Birdtype) :x(x), Birdtype(Birdtype) {}
 	void draw()
 	{
 		if (Birdtype == 1) y = height - 280;
@@ -97,7 +140,8 @@ public:
 	}
 	void move()
 	{
-		x -= bird_speed;
+		x -= bird_speed;//相对运动，让障碍物向左运动
+		//看上去就像恐龙在向右运动
 	}
 };
 
@@ -109,16 +153,16 @@ class Dino
 public:
 	float x, y;
 	float vy, g;
-	bool Jumping=false;//检测是否在跳跃
-	bool Ducking=false;//检测是否在俯下身子或空中俯冲
-	Dino(float x, float y, float vy, float g) :x(x),y(y),vy(vy),g(g){}//构造函数，数据初始化
+	bool Jumping = false;//检测是否在跳跃
+	bool Ducking = false;//检测是否在俯下身子或空中俯冲
+	Dino(float x, float y, float vy, float g) :x(x), y(y), vy(vy), g(g) {}//构造函数，数据初始化
 	void draw()
 	{
 		if (Jumping) putimage1(x, y, &img_dinojump);
 		else if (Ducking)
 		{
-			if (dino_index == 0) putimage1(x, y+30, &img_ducking1);//与小仙人掌相同的问题
-			if (dino_index == 1) putimage1(x, y+30, &img_ducking2);//ducking的图片高度不够
+			if (dino_index == 0) putimage1(x, y + 30, &img_ducking1);//与小仙人掌相同的问题
+			if (dino_index == 1) putimage1(x, y + 30, &img_ducking2);//ducking的图片高度不够
 			//导致恐龙悬浮在空中，所以我们+30
 		}
 		else
@@ -141,7 +185,7 @@ public:
 	{
 		if (Jumping)
 		{
-			vy += g;
+			vy += g;//基本物理逻辑
 			y += vy;
 			if (y > height - 240)
 			{
@@ -155,7 +199,8 @@ public:
 
 	void StartDuck()
 	{
-		if (Jumping) vy += 35;
+		if (Jumping) vy += 35;//如果正在跳跃的过程中按下键，则加速下坠
+		//这时并不会蹲下
 		else Ducking = true;
 	}
 
@@ -190,7 +235,7 @@ vector<Bird> bird;
 //我们在新建仙人掌的部分设计了两个自由度
 //1.随机数1从六种仙人掌中随机选一个
 //2.随机数2保证每次生成新仙人掌的时间间隔不确定，表现在游戏中就是每两个仙人掌之间的距离不总是定值
-void AddCac() 
+void AddCac()
 {
 	static unsigned long Cac_interval = 1500; // 初始默认间隔5000ms,5s
 	unsigned long currenttime = GetTickCount();
@@ -204,7 +249,35 @@ void AddCac()
 		cout << "Current interval: " << Cac_interval << " milliseconds" << endl;
 
 		int Cactype = rand() % 6 + 1;//随机数，六种仙人掌任选一个生成
-		cactus.push_back(Cactus(width, height - 240, Cactype));
+
+		switch (Cactype) //这些参数是在另外一个测试代码中一点点调试出来的，调试了三个小时
+		{
+		case 1:
+			cactus.push_back(Cactus(width, height - 240, Cactype, -12, 2, 70, 60, 65, 70));
+			break;
+		case 2:
+			cactus.push_back(Cactus(width, height - 240, Cactype, 10, 2, 90, 90, 90, 85));
+			break;
+		case 3:
+			cactus.push_back(Cactus(width, height - 240, Cactype, 10, 2, 88, 88, 88, 80));
+			break;
+			//因为加载图片是从x，y开始向右下角加载的,所以 + 20是避免小仙人掌加载时悬在空中
+		case 4:
+			cactus.push_back(Cactus(width, height - 220, Cactype, -5, 2, 70, 70, 50, 70));
+			break;
+		case 5:
+			cactus.push_back(Cactus(width, height - 220, Cactype, -2, 2, 78, 67, 67, 78));
+			break;
+		case 6:
+			cactus.push_back(Cactus(width, height - 220, Cactype, 15, 2, 85, 85, 85, 80));
+			break;
+
+		}
+
+
+
+
+
 		lastcactime = currenttime;
 	}
 }
@@ -222,10 +295,11 @@ void AddBird()
 		cout << "Current interval: " << Bird_interval << " milliseconds" << endl;
 
 		int Birdtype = rand() % 2 + 1;//随机数，高鸟和矮鸟
-		bird.push_back(Bird(width,Birdtype));
+		bird.push_back(Bird(width, Birdtype));
 		lastbirdtime = currenttime;
 	}
 }
+
 
 int main()
 {
@@ -234,21 +308,25 @@ int main()
 	ExMessage msg;//
 	startup();
 	initgraph(width, height);//跑酷窗口
-	mciSendString(L"play D:/game/lbr/Dino/music/Summer.mp3 repeat", 0, 0, 0);//播放音乐
+	mciSendString(L"play music/Summer.mp3 repeat", 0, 0, 0);//播放音乐
 	//mciSendString(_T("play bgm repeat from 0"), NULL, 0, NULL);//循环播放
 
 	BeginBatchDraw();//双缓冲
-	Dino dino1(200, height-240, 0, 3.5);
+	Dino dino1(200, height - 240, 0, 3.5);
 
-	while (true)//后续改为！End
+	while (!End)//后续改为！End
 	{
-
+		/*if (Life == 0)
+		{
+			DrawEnd();
+			break;
+		}*/
 
 
 		AddCac();
 		AddBird();
 		static float Score = 0;
-		Score+= Score_Speed;//配合强制类型转换使用
+		Score += Score_Speed;//配合强制类型转换使用
 		while (peekmessage(&msg))//信息输出，上箭头（或space）和下箭头
 		{
 			if (msg.message == WM_KEYDOWN)
@@ -257,12 +335,12 @@ int main()
 				{
 					dino1.StartJump();
 				}
-				else if (msg.vkcode == VK_DOWN)
+				else if (msg.vkcode == VK_DOWN)//检测键盘的下键
 				{
 					dino1.StartDuck();
 				}
 			}
-			else if (msg.message == WM_KEYUP)
+			else if (msg.message == WM_KEYUP)//如果松开下键，停止蹲的动作
 			{
 				if (msg.vkcode == VK_DOWN) dino1.StopDuck();
 			}
@@ -320,8 +398,17 @@ int main()
 		TCHAR scoreStr[20];
 		_stprintf_s(scoreStr, _T("得分: %d"), static_cast<int>(Score));
 		outtextxy(10, 10, scoreStr);
-		if (static_cast<int>(Score)>50+lastSpeedIncreaseScore)
+
+		/*settextcolor(GREEN);
+		settextstyle(25, 0, _T("宋体"));
+		TCHAR lifeStr[20];
+		_stprintf_s(lifeStr, _T("血量: %d"), static_cast<int>(Life));
+		outtextxy(width-100, 10, lifeStr);*/
+
+		if (static_cast<int>(Score) > 50 + lastSpeedIncreaseScore)
 		{
+			//游戏难度随着分数增长而逐渐增加
+			//具体体现在速度加快，我们直接把速度改为全局变量，防止作用域出问题
 			cac_speed += speed_increase;
 			bird_speed += speed_increase;
 			lastSpeedIncreaseScore = static_cast<int>(Score);
@@ -332,6 +419,11 @@ int main()
 		{
 			c.draw();
 			c.move();
+			if (c.crack(dino1.x, dino1.y))
+			{
+				End = true;//仙人掌碰撞，下面的鸟同理
+				break;
+			}
 		}
 		for (auto& b : bird)
 		{
